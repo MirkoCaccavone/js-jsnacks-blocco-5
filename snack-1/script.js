0f0f64185874ae0cc84319724245db6bf97491f4
Mirko <mirkocaccavone03@gmail.com>
2025-01-21 15:06:26 +0100

    [fix] add method

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,4 +3,6 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names[0], names[1], names[2]);
+names.forEach((name) => {
+  console.log(name);
+});
