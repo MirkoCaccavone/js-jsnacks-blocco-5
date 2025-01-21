89f9833d73fa9228f4c28aff2c665507d9775817
Mirko <mirkocaccavone03@gmail.com>
2025-01-21 15:44:50 +0100

    [feat] add find method for snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,10 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const specificStudent = students.find ((student) => {
+  return student.name === 'Marco Lanci';
+});
+
+console.log(specificStudent.class);
+
